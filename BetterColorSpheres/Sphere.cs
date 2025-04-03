a57b1ec4cb4235582c8bd8e89fd993703af4b1f5
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-04-03 19:31:05 +0100

    Add Sphere class

--- BetterColorSpheres/Sphere.cs
+++ BetterColorSpheres/Sphere.cs
@@ -7,6 +7,31 @@ namespace BetterColorSpheres
 {
     public class Sphere
     {
+        public int Raio {get; private set; }
+        public int Contagem {get; private set; } = 0;
+
+        public Color Cor {get; private set; }
+        
+        public Sphere(Color cor, int raio)
+        {
+            Cor = cor;
+            Raio = raio;
+
+        }
+
+        public void Pop()
+        {
+            Raio = 0;
+        }
+
+        public void Throw()
+        {
+            if (Raio > 0)
+            {
+                Contagem ++;
+            }
+        }
+
         
     }
 }
