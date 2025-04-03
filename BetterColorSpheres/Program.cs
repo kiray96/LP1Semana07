3a5a578e0d5a1f931e5b807dfc27d587ff41256e
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-04-03 19:37:33 +0100

    Test classes in main

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -6,7 +6,29 @@ namespace BetterColorSpheres
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Sphere bola = new Sphere(new Color(1, 4, 6), 9);
+            bola.Throw();
+            bola.Pop();
+
+            Sphere bola1 = new Sphere(new Color(2, 5, 7), 8);
+            bola1.Throw();
+            bola1.Throw();
+
+            PrintSphere(bola);
+            PrintSphere(bola1);
+        }
+
+        private static void PrintSphere(Sphere sphere)
+        {
+            Color color = sphere.Cor;
+
+            Console.WriteLine($"Radius: {sphere.Raio}");
+            Console.WriteLine($"throws: {sphere.Contagem}");
+            Console.WriteLine($"Color Red: {color.Red}");
+            Console.WriteLine($"Color Green: {color.Green}");
+            Console.WriteLine($"Color Blue: {color.Blue}");
+            Console.WriteLine($"Alpha: {color.Alpha}");
+            Console.WriteLine($"Grey: {color.Grey}");
         }
     }
 }
